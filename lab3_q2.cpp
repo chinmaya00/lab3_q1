71de2b6631c22993f3b2a552fb22fbc03617760d
chinmaya00 <42206448+chinmaya00@users.noreply.github.com>
2018-08-17 11:34:23 +0530

    Add files via upload

---
+++ lab3_q2.cpp
@@ -0,0 +1,26 @@
+//First include the Library
+#include<iostream>
+using namespace std;
+//Write the main function
+int main()
+{
+//declare variables
+int a,b;
+int sum,diff,mult,div,mod;
+a = 15;
+b = 5;
+//process
+sum = a + b;
+diff = a - b;
+mult = a * b;
+div = a / b;
+mod = a % b;
+//print out the result
+cout << "Sum of a and b is" << sum << endl;
+cout << "Difference of a and b is" << diff << endl;
+cout << "Multiplication of a and b is" << mult << endl;
+cout << "Division of a and b is" << div << endl;
+cout << "Modulus of a and b is" << mod << endl;
+//terminate the program
+return 0;
+}
