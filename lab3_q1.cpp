efabf777d33714829958abdc86e101d77485c186
chinmaya00 <42206448+chinmaya00@users.noreply.github.com>
2018-08-17 11:08:36 +0530

    Add files via upload

---
+++ lab3_q1.cpp
@@ -0,0 +1,19 @@
+//Library
+#include <iostream>
+using namespace std;
+
+int main(){
+        cout << "Hello" << endl;
+       //Declaring variables of all types
+       int value; 
+       int a,b; //Declaring variables together also possible
+       double doubleVar =1.2; //Defining the variables.
+       bool boolVar = true;
+
+       //Displaying the sizes of data types.
+       cout << "size of a = " << sizeof(a) << endl;
+       cout << "size of doubleVar = " << sizeof(doubleVar) << endl;
+       cout << "size of boolVar = " << sizeof(boolVar) << endl;
+       cout << "size of char = " << sizeof(char) << endl;
+}
+
